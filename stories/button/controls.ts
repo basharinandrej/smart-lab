27bc2fc02800b847bf7749df7ef9d382490c0a36
a.basharin <a.basharin@okko.tv>
2024-09-11 19:16:47 +0300

    add docs for button

--- stories/button/controls.ts
+++ stories/button/controls.ts
@@ -5,22 +5,43 @@ import {Props} from "./Button";
 
 export const controls: Controls = {
   label: {
-    control: { type: 'text'},
     description: 'Текст в кнопке',
+    table: {
+      defaultValue: { summary: 'Кнопка' },
+      type: { summary: 'string' },
+    },
   },
   size: {
-    control: { type: 'select'},
     description: 'Размер кнопки',
+    control: { type: 'select'},
+    table: {
+      defaultValue: { summary: SizeButton.Medium },
+      type: { summary: `${SizeButton.Small}, ${SizeButton.Medium}, ${SizeButton.Big}` },
+    },
     options: [SizeButton.Big, SizeButton.Small, SizeButton.Medium],
   },
   type: {
-    control: { type: 'select' },
     description: 'Тип кнопки',
+    control: { type: 'select' },
+    table: {
+      defaultValue: { summary: TypeButton.Primary },
+      type: { summary: `${TypeButton.Primary}, ${TypeButton.Gray}` },
+    },
     options: [TypeButton.Primary, TypeButton.Gray]
   },
   state: {
     control: { type: 'select' },
     description: 'Состояние кнопки',
+    table: {
+      defaultValue: { summary: StateButton.Default },
+      type: { summary: `
+        ${StateButton.Default}, 
+        ${StateButton.Active},
+        ${StateButton.Hover},
+        ${StateButton.Loading},
+        ${StateButton.Disabled}`
+      },
+    },
     options: [
       StateButton.Default,
       StateButton.Loading,
@@ -31,14 +52,23 @@ export const controls: Controls = {
   },
   icon: {
     description: 'Наличие иконки',
+    table: {
+      defaultValue: { summary: 'null'},
+      type: { summary: `HTMLElement` },
+    },
     control: {
       type: 'boolean',
     }
   }
 }
 
-export const defaultProps: Partial<Props> = {
+export const defaultProps: DefaultProps = {
   label: 'Кнопка',
-  size: SizeButton.Medium
+  size: SizeButton.Medium,
+  state: StateButton.Default,
+  icon: false
 }
 type Controls = Partial<ArgTypes<Props>>
+type DefaultProps = Partial<Omit<Props, 'icon'> & {
+  icon: boolean
+}>
