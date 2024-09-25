27adc76fcec4a3ac10402e57bd4f9402b7d09dfb
basharinandrej <basharinandrej@mail.ru>
2024-09-25 23:06:39 +0300

    add typography and font-face

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,11 @@
 import { Preview } from '@storybook/html';
 import {themeDecorator} from "./decorators/theme-decorator";
 
+import '/global.sass'
 import '/variables.sass'
 import '/variables-dark.sass'
 import '/variables-light.sass'
+import '/fonts.sass'
 
 const preview: Preview = {
   tags: ['autodocs'],
