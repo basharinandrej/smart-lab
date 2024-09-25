e2ded484779d6d82dce1e464cd128ee1275dd859
basharinandrej <basharinandrej@mail.ru>
2024-09-25 23:09:12 +0300

    change grey on secondary

--- stories/button/helpers/get-class-names-by-sizes.ts
+++ stories/button/helpers/get-class-names-by-sizes.ts
@@ -4,7 +4,7 @@ import {TypeButton} from "../enums/enums";
 export const getClassNamesByTypes = (type: TypeButton) => {
   const typesMap: Record<TypeButton, string> = {
     [TypeButton.Primary]: 'ds--button--primary',
-    [TypeButton.Gray]: 'ds--button--gray'
+    [TypeButton.Secondary]: 'ds--button--secondary'
   }
 
   return typesMap[type]
