b8b2c748ad6f86c9d9d86ebdf90a4431a9ff3a2f
basharinandrej <basharinandrej@mail.ru>
2024-09-27 21:01:15 +0300

    build release 1.2 again

---
+++ docs/assets/h2-heading-BAIJXbXe.js
@@ -0,0 +1,16 @@
+import{ae as e,af as l,ai as s}from"./index-Dt2Gml2f.js";import{u as m}from"./index-DDlFV5pR.js";import"./iframe-zenpyXZp.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";const t={type:{primary:"Roboto, sans-serif"},weight:{regular:"400",medium:"500",bold:"700"},size:{h2:{medium:32,small:26}}},r="Lorem ipsum dolor sit amet, consectetur adipiscing elit.";function o(i){const n={h1:"h1",p:"p",strong:"strong",...m(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{title:"Typography/H2 Heading"}),`
+`,`
+`,`
+`,e.jsx(n.h1,{id:"h2-heading",children:"H2 Heading"}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Font:"})," Roboto"]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Weight:"})," 400(regular)"]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Font Size:"})," 32px(Medium), 26px(Small)"]}),`
+`,e.jsx(s,{fontSizes:[Number(t.size.h2.medium),Number(t.size.h2.small)],fontWeight:t.weight.regular,sampleText:r,fontFamily:t.type.primary}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Font:"})," Roboto"]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Weight:"})," 500(medium)"]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Font Size:"})," 32px(Medium), 26px(Small)"]}),`
+`,e.jsx(s,{fontSizes:[Number(t.size.h2.medium),Number(t.size.h2.small)],fontWeight:t.weight.medium,sampleText:r,fontFamily:t.type.primary}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Font:"})," Roboto"]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Weight:"})," 700(bold)"]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Font Size:"})," 32px(Medium), 26px(Small)"]}),`
+`,e.jsx(s,{fontSizes:[Number(t.size.h2.medium),Number(t.size.h2.small)],fontWeight:t.weight.bold,sampleText:r,fontFamily:t.type.primary})]})}function j(i={}){const{wrapper:n}={...m(),...i.components};return n?e.jsx(n,{...i,children:e.jsx(o,{...i})}):o(i)}export{r as SampleText,j as default,t as typography};
