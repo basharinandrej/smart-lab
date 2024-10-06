7c44f1d062dfebe6f3c71150ee6bae164985cb98
Andrej Basharin <a.basharin@vkteam.ru>
2024-10-06 20:57:39 +0300

    take out styles into folders

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,11 +2,11 @@ import { Preview } from '@storybook/html';
 import {svgSpriteDecorator} from "./decorators/svg-sprite-decorator";
 import {themeDecorator} from "./decorators/theme-decorator";
 
-import '/global.sass'
-import '/variables.sass'
-import '/variables-dark.sass'
-import '/variables-light.sass'
-import '/fonts.sass'
+import '/styles/global.sass'
+import '/styles/variables.sass'
+import '/styles/variables-dark.sass'
+import '/styles/variables-light.sass'
+import '/styles/fonts.sass'
 
 const preview: Preview = {
   tags: ['autodocs'],
