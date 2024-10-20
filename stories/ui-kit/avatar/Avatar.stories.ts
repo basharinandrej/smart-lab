0279406f471822b70e19ae943bfe33af5c844a66
Andrej Basharin <a.basharin@vkteam.ru>
2024-10-20 18:40:33 +0300

    create file controls

--- stories/ui-kit/avatar/Avatar.stories.ts
+++ stories/ui-kit/avatar/Avatar.stories.ts
@@ -1,64 +1,15 @@
 import {createAvatar, Props} from './Avatar';
 import type {Meta} from '@storybook/html';
 import { Story } from '../types';
-import { AvatarSize } from './enums/enums';
-import { convertMapToControl } from '../../tools/convert-map-to-control';
-
+import {defaultProps, controls} from './controls'
 
 const meta: Meta<{}> = {
   title: 'UI-KIT/Avatar',
   render: (props: Props) => {
     return createAvatar(props)
   },
-  args: {
-    size: AvatarSize.Large,
-    src: 'https://i.pinimg.com/736x/71/81/14/7181149979b9cc345415d3b6465b192b.jpg',
-    isBorder: false,
-    isShowImg: true,
-    stubText: 'ФИ',
-    isHeaderPremium: true
-  },
-  argTypes: {
-    stubText: {
-      description: 'Заглушка',
-      table: {
-        defaultValue: { summary: 'ФИ'},
-        type: { summary: 'string' },
-      },
-    },
-    isBorder: {
-      description: 'Наличие рамки',
-      table: {
-        defaultValue: { summary: false },
-        type: { summary: 'boolean' },
-      },
-    },
-    isHeaderPremium: {
-      description: 'Премиум',
-      table: {
-        defaultValue: { summary: false },
-        type: { summary: 'boolean' },
-      },
-    },
-    isShowImg: {
-      description: 'Показать изображение',
-      table: {
-        defaultValue: { summary: false },
-        type: { summary: 'boolean' },
-      },
-    },
-    size: {
-      description: 'Размер аватар',
-      ...convertMapToControl(AvatarSize)
-    },
-    src: {
-      description: 'URL изображения',
-      table: {
-        defaultValue: { summary: 'Кнопка' },
-        type: { summary: 'string' },
-      },
-    }
-  },
+  args: defaultProps,
+  argTypes: controls,
   parameters: {
     backgrounds: {
       default: 'dark',
